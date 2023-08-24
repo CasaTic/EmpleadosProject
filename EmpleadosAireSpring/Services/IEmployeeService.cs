3db09da86ccce45e7026a3ce2a2eaf216e35ebe2
Evelyn Elena Campos <elenuco36@gmail.com>
2023-08-24 14:37:37 -0600

    feat: sea agregan uevos cambios

---
+++ EmpleadosAireSpring/Services/IEmployeeService.cs
@@ -0,0 +1,12 @@
+﻿using EmpleadosAireSpring.Models;
+
+namespace EmpleadosAireSpring.Services
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Employee> GetAllEmployees();
+        Employee GetEmployeeByID(int employeeID);
+        void AddEmployee(Employee employee);
+        // Otras operaciones según necesidad
+    }
+}
